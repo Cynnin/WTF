acd2b5d396c591e9f1b3d3d81565231f2dff0bf9
Cynnin <cynone63@gmail.com>
2025-09-07 17:50:00 +0100

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,6 @@
+const myName= prompt("Enter your name");
+alert("Hello " + myName + " Welcome to my website");
+console.log("Hello " + myName + " Welcome to my website");
+let jobTitle= prompt("Enter your job title");
+alert("You are a " + jobTitle);
+console.log("You are a " + jobTitle);
---
+++ Week 1 - September.js
@@ -0,0 +1,13 @@
+let num1 = 45;
+let num2 = 15;
+let name = 'Heartqueen';
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+console.log('Addition: ' + addition);
+console.log('Subtraction: ' + subtraction);
+console.log('Multiplication: ' + multiplication);
+console.log('Division: ' + division);
+console.log("Hello " + name + " the result of num1 and num2 is " + addition);
